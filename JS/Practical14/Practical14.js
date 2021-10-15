f249fc9461949fe471c5e4b2d86516a477e64f70
kshitij.maheshwari_cs19 <kshitij.maheshwari_cs19@gla.ac.in>
2021-10-15 11:01:40 +0530

    created Practical14

---
+++ JS/Practical14/Practical14.js
@@ -0,0 +1,24 @@
+var counter = 0;
+var x = document.getElementById("text");
+
+function fun(){
+    counter++;
+    x.innerHTML = counter;
+
+}
+
+function fun1() {
+    counter--;
+   if (counter <= 0) {
+       counter = 0;
+   }
+   x.innerHTML = counter;
+}
+
+function mouseHover(active) {
+    active.style.borderColor = "black";
+}
+
+function mouseOut(y) {
+    y.style.borderColor = "lightgrey";
+}
